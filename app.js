573de185e4bfeea27e59f2b473aeae88846458a2
Jason Miller <jasonmiller.dev@gmail.com>
2022-08-11 16:12:35 +1000

    Div colouring functionality working

--- app.js
+++ app.js
@@ -1,16 +1,11 @@
 const gridContainer = document.querySelector(".grid-container");
 let divSize = 30;
-let divCount = 16;
+let divCount = 16 * 16;
 
 function divGenerate(divCount) {
-  let divCountSpread = String(divCount)
-    .split("")
-    .map((divCount) => {
-      return Number(divCount);
-    });
-  console.log(divCountSpread);
-  for (let i = 0; i <= divCountSpread; i++) {
-    console.log(i);
+  const countArray = Array.from({ length: divCount }, (_, index) => index + 1);
+  console.log(countArray);
+  for (let i = 0; i < countArray.length; i++) {
     drawDiv();
   }
 }
@@ -19,11 +14,24 @@ divGenerate(divCount);
 
 function drawDiv() {
   const newDiv = document.createElement("div");
-  newDiv.style.backgroundColor = "blue";
+  newDiv.style.backgroundColor = "white";
   newDiv.style.height = `${divSize}px`;
   newDiv.style.width = `${divSize}px`;
   gridContainer.appendChild(newDiv);
 }
 
-drawDiv();
-drawDiv();
+// document.addEventListener("DOMContentLoaded", function () {
+//   // Handler for when the DOM is fully loaded
+// });
+
+gridContainer.addEventListener("mouseover", (event) => {
+  console.log(event);
+  event.target.style.backgroundColor = "red";
+  //   let allHoverEvents = document.querySelectorAll(".div");
+
+  //   for (let i = 0; i < allHoverEvents.length; i++) {
+  //     allHoverEvents.item(i).onmouseover = function () {
+  //       console.log("Hello!");
+  //     };
+  //   }
+});
