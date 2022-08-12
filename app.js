04002a5212e9175f8b064809d933072764d36be4
Jason Miller <jasonmiller.dev@gmail.com>
2022-08-13 05:25:11 +1000

    Starting functionality again

--- app.js
+++ app.js
@@ -1,37 +1,29 @@
 const gridContainer = document.querySelector(".grid-container");
 let divSize = 30;
-let divCount = 16 * 16;
+let divCount = 220;
 
-function divGenerate(divCount) {
+// Loops through
+function divGenerate() {
   const countArray = Array.from({ length: divCount }, (_, index) => index + 1);
   console.log(countArray);
+
   for (let i = 0; i < countArray.length; i++) {
     drawDiv();
   }
 }
 
-divGenerate(divCount);
+divGenerate();
 
 function drawDiv() {
   const newDiv = document.createElement("div");
   newDiv.style.backgroundColor = "white";
   newDiv.style.height = `${divSize}px`;
   newDiv.style.width = `${divSize}px`;
+
   gridContainer.appendChild(newDiv);
 }
 
-// document.addEventListener("DOMContentLoaded", function () {
-//   // Handler for when the DOM is fully loaded
-// });
-
 gridContainer.addEventListener("mouseover", (event) => {
   console.log(event);
-  event.target.style.backgroundColor = "red";
-  //   let allHoverEvents = document.querySelectorAll(".div");
-
-  //   for (let i = 0; i < allHoverEvents.length; i++) {
-  //     allHoverEvents.item(i).onmouseover = function () {
-  //       console.log("Hello!");
-  //     };
-  //   }
+  event.target.style.backgroundColor = "black";
 });
